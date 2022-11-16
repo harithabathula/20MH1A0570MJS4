496d8c8cb78e49f83d25132bf10c72654a0667ba
ramasubbaiah <ramasubbaiah@LAPTOP-8FTKGC45>
2022-11-16 20:38:28 +0530

    MOVING MAVEN FROM ECLIPSE TO GIT

---
+++ src/main/java/DEVOPSLAB/MH1A0570MJS4/App.java
@@ -0,0 +1,13 @@
+package DEVOPSLAB.MH1A0570MJS4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
